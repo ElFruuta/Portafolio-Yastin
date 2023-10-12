40bd655930390aa3a2446a8ec987a336f78f599d
elfruutas <yastin.villarroel.cancino@alumnos.sip.cl>
2023-10-12 09:41:44 -0300

    AVANZA

--- script.js
+++ script.js
@@ -27,14 +27,24 @@ window.onscroll = function () {
 
 
     var menu = document.getElementById("menu");
-
     
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         menu.style.backgroundColor = "#fff"; // Cambia el fondo a blanco al hacer scroll
         menu.style.color = "#000"; // Cambia el color del texto a negro al hacer scroll
+        var items_menu = menu.getElementsByClassName("menu"); // Cambiar el color de los items a negro al hacer scroll
+        for (const item of items_menu) {
+            item.style.color = "#000";
+        }
+
     } else {
         menu.style.backgroundColor = "#000"; // Restaura el fondo original al llegar arriba
         menu.style.color = "white"; // Restaura el color del texto original al llegar arriba
+        
+        // Cambiar el color de los items a blanco al hacer scroll
+        var items_menu = menu.getElementsByClassName("menu"); 
+        for (const item of items_menu) {
+            item.style.color = "#fff";
+        }
     }
 };
 
@@ -55,24 +65,4 @@ document.querySelectorAll('a.menu').forEach(function (link) {
         }
     });
 });
-
-const content = document.getElementById("content");
-let isScrollingDown = false;
-
-window.addEventListener("scroll", function () {
-    
-    const scrollY = window.scrollY;
-    if (!isScrollingDown && scrollY > 0) {
-        
-        content.style.color = "#fff"; // Cambia el color de texto
-        content.style.backgroundColor = "#000"; // Cambia el color de fondo
-        isScrollingDown = true;
-    } else if (isScrollingDown && scrollY === 0) {
-        
-        content.style.color = "#000"; // Restaura el color de texto original
-        content.style.backgroundColor = "#fff"; // Restaura el color de fondo original
-        isScrollingDown = false;
-    }
-});
-
 
